305282bc2c59dc88f34ce0f7efb628ba7565c357
leeseoku <leeseoku@naver.com>
2023-05-12 10:25:41 +0900

    2023 05 12

---
+++ js/basic.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){})
+
+
+
+// // 선택자 > 이벤트 > 메서드
+
+// $("#slider li").click(function(){})
+
+// // first-child, :nth-child(1), last-child
+
+// $("slider li:nth-child(1) > a").click(function(){})
+
+// var num = 0;
+// $("slider li").eq(num).find('>a').click(function(){})
+// 슬라이더 li 자손 num번째 안에 있는 a를 찾아서 클릭하면 실행해라
+
+
+// 알아둬야 하는 메서트
+// 주어용 메서드 : eq(숫자) , find(자식객체)
+// 동사용 메서드 : addClass(클래스명), removeClass(클래스명), togleClass(클래스명)
+
+// 알아둬야 하는 이벤트
+// ready : 선택가자 오직 하나 document
+// click : 모든 객체 해당됨
+// submit : 폼에 있는 button, input만 해당
+// change : 폼에 있는 input만 해당
+
+
+
+
+
+
+
